a039623ce032cb9f5677956da6ad8dd4b78e9ad3
jelle-cr <126500480+jelle-cr@users.noreply.github.com>
2023-03-06 15:53:33 +0100

    Homework 1 attempted constrained and unconstrained MPC not working

--- Homework 1/Assignment1.m
+++ Homework 1/Assignment1.m
@@ -4,6 +4,7 @@ close all;
 clear all;
 clc
 
+t = 300; 
 Ts = 0.1;
 N = 20;
 
@@ -20,22 +21,126 @@ B = ss_d.B;
 C = ss_d.C;
 D = ss_d.D;
 [n,m] = size(B);
+p = height(C);
 
-[phi, gamma] = predictionModel(A,B,N,n,m);
-
-%% Cost function (Likely incorrect due to the cost function being different from the instructions)
-Q = 10*eye(n);
-R = 0.1*eye(m);
-[~,P,~] = dlqr(A,B,Q,R);                    % P is the solution of the ARE in discrete-time
-
-omega = blkdiag(kron(eye(N-1),Q), P);       % Kronecker product
-psi = kron(eye(N),R);
-
-G = 2*(psi+gamma'*omega*gamma);
-F = 2*gamma'*omega*phi;
-
-%% Question 4.2 (design a model predictive controller)
+% Initial conditions
 h0 = 0;
 v0 = 10;
 x0 = [v0; -2.2541; -2.23e-17; -0.2912];
 u0 = [0.3792; 0.0203];
+y0 = [v0; h0];
+
+ref = zeros(2,t+2*N);
+ref(1,1:200) = 10;
+ref(1,201:end) = 8;
+ref(2,1:100) = 0;
+ref(2,101:end) = 5;
+ref = reshape(ref,2*(t+2*N),1);
+
+%% Constraints and matrices
+constr.statelb = [-25;-25;-pi/20;-pi/2;-15]; % v,w,q,theta,h
+constr.stateub =  [25;25;pi/20;pi/2;15]; % v,w,q,theta,h
+constr.initialstatelb = -abs([x0; 0]); % v,w,q,theta,h
+constr.initialstateub = abs([x0; 0]);
+constr.terminalstatelb = [-25;-25;-pi/20;-pi/2;-15]; % Change if needed after monday
+constr.terminalstateub = [25;25;pi/20;pi/2;15];
+constr.inputlb = [-20;-20];
+constr.inputub =  [20;20];
+constr.deltainputlb = [-15;-15];
+constr.deltainputub =  [15;15];
+
+Q = 10*eye(p);
+R = 0.1*eye(m);
+
+[phi, gamma] = predictionModel(A,B,N,n,m);
+omega = kron(eye(N),Q);       % Kronecker product
+psi = kron(eye(N),R);
+C_bar = kron(eye(N),C);
+
+T = diag(ones(N*m,1));
+nT = size(T,1);
+T(2:nT+1:end) = -1;
+
+[W,L,c,S] = getWLcS(constr,N,B,gamma,phi,T);
+
+%% Question 4.2 (design a model predictive controller)
+k_sim = t;
+%% unconstrained
+G = 2*(gamma'*C_bar'*omega*C_bar*gamma+T'*psi*T);
+F = 2*gamma'*C_bar'*omega*C_bar*phi;
+v = [u0 ; zeros(2*(N-1),1)];
+xk = [x0 zeros(n,k_sim)];
+uk = [zeros(m,1) u0 zeros(m,t-1)];
+yk = [y0 zeros(p,k_sim)];       %output at each time index k
+for k = 1:k_sim
+    %F = 2*(gamma'*C_bar'*omega*C_bar*phi*xk(:,1)-gamma'*C_bar'*omega*ref(k:(k-1+2*N))-2*T'*psi*v);
+
+    K_mpc = -[eye(m) zeros(m,p*(N-1))]*G^-1*F;
+    uk(:,k) = K_mpc*xk(:,k);
+    xk(:,k+1) = A*xk(:,k)+B*uk(:,k);
+    yk(:,k+1) = C*xk(:,k);
+end
+figure()
+subplot(1,2,1)
+stairs(0:t,yk(1,:))
+xlabel('$k$','Interpreter','latex');
+ylabel('$v [ft/sec]$','Interpreter','latex');
+
+subplot(1,2,2)
+stairs(0:t,yk(2,:))
+xlabel('$k$','Interpreter','latex');
+ylabel('$h [ft/sec]$','Interpreter','latex');
+sgtitle('Outputs')
+
+figure()
+subplot(1,2,1)
+stairs(0:t,uk(1,:))
+xlabel('$k$','Interpreter','latex');
+ylabel('$e$','Interpreter','latex');
+
+subplot(1,2,2)
+stairs(0:t,uk(2,:))
+xlabel('$k$','Interpreter','latex');
+ylabel('$\tau$','Interpreter','latex');
+sgtitle('Inputs')
+
+%% constrained
+% xk = [x0 zeros(n,k_sim)];       %state at each time index k
+% yk = [y0 zeros(p,k_sim)];       %output at each time index k
+% uk = [zeros(size(u0)),u0,zeros(m,t-1)];
+% xk(:,2) = A*xk(:,1)+B*uk(:,1); % Calculate x1 (because we know u0)
+% yk(:,2) = C*xk(:,1);
+% v = [u0 ; zeros(2*(N-1),1)];
+% H = 2*(gamma'*C_bar'*omega*C_bar*gamma+T'*psi*T);
+% f = 2*(gamma'*C_bar'*omega*C_bar*phi*xk(:,1)-gamma'*C_bar'*omega*ref(1:2*N)-2*T'*psi*v);
+% c = c+S*v; % update c to new constraints
+% check = 0;
+% for k = 2:t
+%     [Uk,fval,exitflag] = quadprog(H,f,L,c+W*xk(:,k),[],[],[],[],[],[]);
+%     if exitflag ~= 1
+%         warning('exitflag quadprog = %d\n', exitflag)
+%         if exitflag == -2
+%             sprintf('Optimization problem is infeasible.')
+%         end
+%     end
+%     uk(:,k) = Uk(1:m);
+%     xk(:,k+1) = A*xk(:,k)+B*uk(:,k);
+%     yk(:,k+1) = C*xk(:,k);
+%     v = [uk(:,k) ; zeros(2*(N-1),1)];
+%     c = c+S*v; 
+%     f = 2*(gamma'*C_bar'*omega*C_bar*phi*xk(:,1)-gamma'*C_bar'*omega*ref(k:(k-1+2*N))-2*T'*psi*v);
+%     check = check+1
+% end
+% % 
+% figure()
+% subplot(1,2,1)
+% stairs(0:t,yk)
+% xlabel('$k$','Interpreter','latex');
+% ylabel('$y$','Interpreter','latex');
+% 
+% subplot(1,2,2)
+% stairs(0:t,uk)
+% xlabel('$k$','Interpreter','latex');
+% ylabel('$u$','Interpreter','latex');
+% 
+% 
--- Instruction 2/Instruction_2.m
+++ Instruction 2/Instruction_2.m
@@ -72,77 +72,81 @@ F = 2*Gamma'*Omega*Phi;
 % McalTest = zeros((height(umin)+height(umax)+height(xmin)+height(xmax))*N+height(xmin)+height(xmax),N*nx);
 % EcalTest = zeros((height(umin)+height(umax)+height(xmin)+height(xmax))*N+height(xmin)+height(xmax),N*nu);
 
-% %% (c) Closed-loop trajectory
-% k_sim = 500;
-% xk = [x0 zeros(nx,k_sim)];
-% uk = zeros(nu,k_sim);
-% Uk = zeros(nu*N,k_sim);
-% opt =  optimoptions('quadprog','Display','off');
-% warning('off','optim:quadprog:HessianNotSym')
-% for k = 1:k_sim
-%     [U,~,exitflag] = quadprog(G,F*xk(:,k),L,c+W*xk(:,k),[],[],[],[],[],opt);
-%     if exitflag ~= 1
-%         warning('exitflag quadprog = %d\n', exitflag)
-%         if exitflag == -2
-%             sprintf('Optimization problem is infeasible.')
-%         end
-%     end
-%     Uk(:,k) = U;
-%     uk(:,k) = U(1:nu);
-%     xk(:,k+1) = A*xk(:,k)+B*uk(:,k);
-% end
-% 
-% figure
-% subplot(1,2,1);
-% stairs(0:k_sim,xk')
-% xlabel('$k$','Interpreter','latex');
-% ylabel('$x$','Interpreter','latex');
-% xlim([0 k_sim]);
-% 
-% subplot(1,2,2);
-% stairs(0:k_sim-1,uk)
-% xlabel('$k$','Interpreter','latex');
-% ylabel('$u$','Interpreter','latex');
-% xlim([0 k_sim]);
-% ylim([umin-0.1 umax+0.1]);
-% sgtitle('Constrained MPC')
-% 
-% % (d) Comparison with open loop predicted trajecotry
-% % Follow the previous section
-% figure
-% hold on
-% for k = 1:k_sim
-%     stairs(k-1:k+N-2,Uk(:,k))
-% end
-% stairs(0:k_sim-1,uk,'k')
-% xlabel('$k$','Interpreter','latex');
-% ylabel('$u$','Interpreter','latex');
-% xlim([0 k_sim]);
-% ylim([umin-0.1 umax+0.1]);
-% title('Open loop predicted trajecotry of input')
-% 
-% % (e) Comparison with saturated unconstrained MPC
-% K_mpc = -[eye(nu) zeros(nu,N-1)]*G^-1*F;
-% k_sim = 500;
-% xk = [x0 zeros(nx,k_sim)];
-% uk = zeros(nu,k_sim);
-% for k = 1:k_sim
-%     uk(:,k) = K_mpc*xk(:,k);
-%     uk(:,k) = min(max(uk(:,k),umin),umax); % saturation -[1,1]
-%     xk(:,k+1) = A*xk(:,k)+B*uk(:,k);
-% end
-% 
-% figure
-% subplot(1,2,1);
-% stairs(0:k_sim,xk')
-% xlabel('$k$','Interpreter','latex');
-% ylabel('$x$','Interpreter','latex');
-% xlim([0 k_sim]);
-% 
-% subplot(1,2,2);
-% stairs(0:k_sim-1,uk)
-% xlabel('$k$','Interpreter','latex');
-% ylabel('$u$','Interpreter','latex');
-% xlim([0 k_sim]);
-% ylim([umin-0.1 umax+0.1]);
-% sgtitle('Saturated unconstrained MPC')
+%% (c) Closed-loop trajectory
+k_sim = 500;
+xk = [x0 zeros(nx,k_sim)];
+uk = zeros(nu,k_sim);
+Uk = zeros(nu*N,k_sim);
+opt =  optimoptions('quadprog','Display','off');
+warning('off','optim:quadprog:HessianNotSym')
+for k = 1:k_sim
+    [U,~,exitflag] = quadprog(G,F*xk(:,k),L,c+W*xk(:,k),[],[],[],[],[],opt);
+    if exitflag ~= 1
+        warning('exitflag quadprog = %d\n', exitflag)
+        if exitflag == -2
+            sprintf('Optimization problem is infeasible.')
+        end
+    end
+    sizeFquadprog = size(F*xk(:,k));
+    Uk(:,k) = U;
+    uk(:,k) = U(1:nu);
+    xk(:,k+1) = A*xk(:,k)+B*uk(:,k);
+end
+
+figure
+subplot(1,2,1);
+stairs(0:k_sim,xk')
+xlabel('$k$','Interpreter','latex');
+ylabel('$x$','Interpreter','latex');
+xlim([0 k_sim]);
+
+subplot(1,2,2);
+stairs(0:k_sim-1,uk)
+xlabel('$k$','Interpreter','latex');
+ylabel('$u$','Interpreter','latex');
+xlim([0 k_sim]);
+ylim([umin-0.1 umax+0.1]);
+sgtitle('Constrained MPC')
+
+% (d) Comparison with open loop predicted trajecotry
+% Follow the previous section
+figure
+hold on
+for k = 1:k_sim
+    stairs(k-1:k+N-2,Uk(:,k))
+end
+stairs(0:k_sim-1,uk,'k')
+xlabel('$k$','Interpreter','latex');
+ylabel('$u$','Interpreter','latex');
+xlim([0 k_sim]);
+ylim([umin-0.1 umax+0.1]);
+title('Open loop predicted trajecotry of input')
+
+%% (e) Comparison with saturated unconstrained MPC
+K_mpc = -[eye(nu) zeros(nu,N-1)]*G^-1*F;
+K_mpcsize = size(K_mpc)
+Gsize = size(G)
+Fsize = size(F)
+k_sim = 500;
+xk = [x0 zeros(nx,k_sim)];
+uk = zeros(nu,k_sim);
+for k = 1:k_sim
+    uk(:,k) = K_mpc*xk(:,k);
+    uk(:,k) = min(max(uk(:,k),umin),umax); % saturation -[1,1]
+    xk(:,k+1) = A*xk(:,k)+B*uk(:,k);
+end
+
+figure
+subplot(1,2,1);
+stairs(0:k_sim,xk')
+xlabel('$k$','Interpreter','latex');
+ylabel('$x$','Interpreter','latex');
+xlim([0 k_sim]);
+
+subplot(1,2,2);
+stairs(0:k_sim-1,uk)
+xlabel('$k$','Interpreter','latex');
+ylabel('$u$','Interpreter','latex');
+xlim([0 k_sim]);
+ylim([umin-0.1 umax+0.1]);
+sgtitle('Saturated unconstrained MPC')
